888335f641e1c87f1c601481ab5745982b3b2df9
fuatozerr <fuatozerr23@gmail.com>
2018-08-05 14:37:03 +0300

    Genel Ayarlar Yapıldı

---
+++ database/seeds/AyarTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Ayar;
+use Illuminate\Database\Seeder;
+
+class AyarTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Ayar::create(["name"=>"baslik","value"=>"Yazılım Dünyası"]);
+        Ayar::create(["name"=>"author","value"=>"Fuat Özer"]);
+        Ayar::create(["name"=>"aciklama","value"=>"Az kod çok iş"]);
+        Ayar::create(["name"=>"keywords","value"=>"yazılım,php,java"]);
+        Ayar::create(["name"=>"facebook","value"=>"http://www.facebook.com/fuatozerr"]);
+        Ayar::create(["name"=>"twitter","value"=>"http://www.twitter.com/fuatozerr"]);
+        Ayar::create(["name"=>"github","value"=>"http://www.github.com/fuatozerr"]);
+    }
+}
